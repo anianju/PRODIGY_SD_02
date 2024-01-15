dc8f52b5eab0a4e60d08e947f84be36f03b1b8ee
Anjan C <108664809+anianju@users.noreply.github.com>
2024-01-15 16:29:07 +0530

    Add files via upload

---
+++ TemperatureConverter1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class TemperatureConverter1 {
+    public static double celsiusToFahrenheit(double celsius) {
+        return (celsius * 9 / 5) + 32;
+    }
+
+    public static double celsiusToKelvin(double celsius) {
+        return celsius + 273.15;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter temperature in Celsius: ");
+        double celsius = scanner.nextDouble();
+
+        double fahrenheit = celsiusToFahrenheit(celsius);
+        double kelvin = celsiusToKelvin(celsius);
+
+        System.out.println(celsius + " Celsius is equal to " + fahrenheit + " Fahrenheit");
+        System.out.println(celsius + " Celsius is equal to " + kelvin + " Kelvin");
+
+        scanner.close();
+    }
+}
